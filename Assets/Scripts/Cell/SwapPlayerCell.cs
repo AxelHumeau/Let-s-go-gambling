c999474305e512b7452b45a1ad8acc0ab23dbe0a
Axel Humeau <axel.humeau@epitech.eu>
2025-11-30 18:26:35 +0100

    feat: add cell types

---
+++ Assets/Scripts/Cell/SwapPlayerCell.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class SwapPlayerCell : MonoBehaviour, ICellType
+{
+    public string GetCellName() => "Swap Players";
+
+    public void OnStopOnCell(Cell cell, Player player)
+    {
+        var players = FindAnyObjectByType<TurnManager>().Players.Where(p => p != player);
+        int index = Random.Range(0, players.Count());
+        Player targetPlayer = players.ElementAt(index);
+        Cell targetCell = targetPlayer.CurrentCell;
+        targetPlayer.CurrentCell = cell;
+        player.CurrentCell = targetCell;
+    }
+
+    public void OnPassOnCell(Cell cell, Player player)
+    {
+        // No action for empty cell
+    }
+}
